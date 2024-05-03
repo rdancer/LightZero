8f5be79d69a5d78cddf78a8e129c86dece7d5f72
Jan Minar <rdancer@rdancer.org>
2024-05-03 09:05:59 +0100

    Update martis_game_stochastic_muzero_config_tiny.py

--- zoo/assembly/martis_game/config/martis_game_stochastic_muzero_config_tiny.py
+++ zoo/assembly/martis_game/config/martis_game_stochastic_muzero_config_tiny.py
@@ -10,7 +10,7 @@ evaluator_env_num = 5
 num_simulations = 25
 update_per_collect = 50
 batch_size = 256
-max_env_step = int(2e5)
+max_env_step = int(1e5)
 reanalyze_ratio = 0
 # ==============================================================
 # end of the most frequently changed config specified by the user
